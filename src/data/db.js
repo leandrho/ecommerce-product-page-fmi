697ed56addab98f044090414607475bbbad55f13
Leandrho <leandro.coder01@gmail.com>
2024-09-27 17:11:51 -0300

    add data file

---
+++ src/data/db.js
@@ -0,0 +1,18 @@
+export const products = [
+        {
+            id: 1,
+            name: "Fall Limited Edition Sneakers",
+            description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+            company: "Sneaker Company",
+            price: 250,
+            discount: 50,
+            thumb1: './images/image-product-1-thumbnail.jpg',
+            thumb2: './images/image-product-2-thumbnail.jpg',
+            thumb3: './images/image-product-3-thumbnail.jpg',
+            thumb4: './images/image-product-4-thumbnail.jpg',
+            image1: './images/image-product-1.jpg',
+            image2: './images/image-product-2.jpg',
+            image3: './images/image-product-3.jpg',
+            image4: './images/image-product-4.jpg',
+        }
+    ]
